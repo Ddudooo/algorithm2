145a12272ea25d240c13b27405636ca62a97af95
Ddudooo <igoa.work@gmail.com>
2022-03-31 23:11:05 +0900

    인프런 문자열 문제 2. 대소문자 변환 문제 추가

---
+++ inflearn/Strings/problem01-02/javaSolution/src/main/java/study/problem01_02/Application.java
@@ -0,0 +1,14 @@
+package study.problem01_02;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int input1 = in.nextInt();
+		int input2 = in.nextInt();
+		System.out.println(input1 + input2);
+		return;
+	}
+}
