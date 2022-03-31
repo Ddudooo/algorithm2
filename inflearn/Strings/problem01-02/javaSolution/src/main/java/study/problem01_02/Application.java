71712a5083658fbcd267be47e3d19605f1e2149c
Ddudooo <igoa.work@gmail.com>
2022-03-31 23:44:30 +0900

    인프런 문자열 문제 2. 대소문자 변환 풀이 추가

--- inflearn/Strings/problem01-02/javaSolution/src/main/java/study/problem01_02/Application.java
+++ inflearn/Strings/problem01-02/javaSolution/src/main/java/study/problem01_02/Application.java
@@ -1,14 +1,28 @@
 package study.problem01_02;
 
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toLowerCase;
+import static java.lang.Character.toUpperCase;
+
+import java.io.StringWriter;
 import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int input1 = in.nextInt();
-		int input2 = in.nextInt();
-		System.out.println(input1 + input2);
-		return;
+		String input = in.next();
+		String answer = solution(input);
+		System.out.println(answer);
+	}
+
+	public static String solution(String input) {
+		return input.chars()
+			.collect(
+				StringWriter::new,
+				(sw, ch) -> sw.write((isUpperCase(ch) ? toLowerCase(ch) : toUpperCase(ch))),
+				(swl, swr) -> swl.write(swr.toString())
+			)
+			.toString();
 	}
 }
