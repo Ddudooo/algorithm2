07eb0306cd7e3bbcf1e01ac3b1d76e49ede1144f
Jungdoo Lee <47135476+Ddudooo@users.noreply.github.com>
2022-10-30 17:40:21 +0900

    add solution code (#148)

--- leetcode/medium/22._Generate_Parentheses/javaSolution/src/main/kotlin/study/algorithm2/leetcode/medium/generate_parentheses/Solution.kt
+++ leetcode/medium/22._Generate_Parentheses/javaSolution/src/main/kotlin/study/algorithm2/leetcode/medium/generate_parentheses/Solution.kt
@@ -1,7 +1,38 @@
 package study.algorithm2.leetcode.medium.generate_parentheses
 
+private const val OPEN_PARENT = "("
+private const val CLOSE_PARENT = ")"
+
 class Solution {
+
     fun generateParenthesis(n: Int): List<String> {
+        val result = mutableListOf<String>()
+
+        generate("", 0, 0, n, result)
+        return result
+    }
+
+    private fun generate(initStr: String, open: Int, close: Int, n: Int, list: MutableList<String>) {
+        if (open == n && close == n) {
+        
+            list.add(initStr)
+        }
+
+        if (close > open) {
+
+            return
+        }
+
+        if (open > n || close > n) {
+
+            return
+        }
+
+
+        generate("$initStr$OPEN_PARENT", open + 1, close, n, list)
+
+
+        generate("$initStr$CLOSE_PARENT", open, close + 1, n, list)
 
     }
 }
