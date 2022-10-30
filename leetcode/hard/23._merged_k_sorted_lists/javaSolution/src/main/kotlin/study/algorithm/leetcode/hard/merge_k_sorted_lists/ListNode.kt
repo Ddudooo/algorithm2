3f8e127b0c3f81ed8444d62b1113340a151d6f34
Ddudooo <igoa.work@gmail.com>
2022-10-30 21:24:28 +0900

    add problem code

---
+++ leetcode/hard/23._merged_k_sorted_lists/javaSolution/src/main/kotlin/study/algorithm/leetcode/hard/merge_k_sorted_lists/ListNode.kt
@@ -0,0 +1,5 @@
+package study.algorithm.leetcode.hard.merge_k_sorted_lists
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
---
+++ leetcode/hard/23._merged_k_sorted_lists/javaSolution/src/main/kotlin/study/algorithm/leetcode/hard/merge_k_sorted_lists/Solution.kt
@@ -0,0 +1,7 @@
+package study.algorithm.leetcode.hard.merge_k_sorted_lists
+
+class Solution {
+    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+        TODO("")
+    }
+}
