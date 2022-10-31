8b0adf18d201b4d32a856966e8a4bd8b3153d1f8
Jungdoo Lee <47135476+Ddudooo@users.noreply.github.com>
2022-10-31 23:37:01 +0900

    add solution codes (#150)

--- leetcode/medium/24._Swap_Nodes_in_Pairs/javaSolution/src/main/kotlin/study/algorithm/leetcode/medium/swap_nodes_in_pairs/Solution.kt
+++ leetcode/medium/24._Swap_Nodes_in_Pairs/javaSolution/src/main/kotlin/study/algorithm/leetcode/medium/swap_nodes_in_pairs/Solution.kt
@@ -2,6 +2,22 @@ package study.algorithm.leetcode.medium.swap_nodes_in_pairs
 
 class Solution {
     fun swapPairs(head: ListNode?): ListNode? {
-        TODO("")
+        head ?: return null
+
+        var pointer: ListNode? = head
+        pointer?.next ?: return head
+        var cnt = 0
+
+        while (pointer != null) {
+            if (cnt % 2 == 0 && pointer.next != null) {
+                val originVal = pointer.`val`
+                pointer.`val` = pointer.next!!.`val`
+                pointer.next!!.`val` = originVal
+            }
+            pointer = pointer.next
+            cnt++
+        }
+
+        return head
     }
 }
