353e7220a85e984d79eae9a1b8719318fc12ca42
Ddudooo <igoa.work@gmail.com>
2023-04-26 01:51:27 +0900

    add solution source

--- leetcode/easy/27_remove_element/javaSolution/27_remove_element_solution/src/main/kotlin/study/algorithm/leetcode/easy/remove_element/Solution.kt
+++ leetcode/easy/27_remove_element/javaSolution/27_remove_element_solution/src/main/kotlin/study/algorithm/leetcode/easy/remove_element/Solution.kt
@@ -2,6 +2,8 @@ package study.algorithm.leetcode.easy.remove_element
 
 class Solution {
     fun removeElement(nums: IntArray, `val`: Int): Int {
-        TODO()
+        var targetIdx = 0
+        nums.forEach { if(it!=`val`) nums[targetIdx++] = it }
+        return targetIdx
     }
 }
--- leetcode/easy/27_remove_element/javaSolution/27_remove_element_solution/src/test/kotlin/study/algorithm/leetcode/easy/remove_element/SolutionTest.kt
+++ leetcode/easy/27_remove_element/javaSolution/27_remove_element_solution/src/test/kotlin/study/algorithm/leetcode/easy/remove_element/SolutionTest.kt
@@ -2,6 +2,8 @@ package study.algorithm.leetcode.easy.remove_element
 
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
@@ -10,5 +12,21 @@ class SolutionTest: StringSpec( {
         shouldNotThrow<Exception> {
             Solution()
         }
+    }
+
+    "배열로 [3,2,2,3]이 주어지고 3이 주어지면 2를 반환하고 배열은 [2,2,_,_] 형태로 바뀌어야 한다" {
+        val nums = intArrayOf(3, 2, 2, 3)
+        val result = Solution().removeElement(nums, 3)
+
+        result shouldBe 2
+        nums.slice(0..result) shouldNotContain 3
+    }
+
+    "배열로 [0,1,2,2,3,0,4,2]이 주어지고 2이 주어지면 5를 반환하고 배열은 [0,1,4,0,3,_,_,_] 형태로 바뀌어야 한다" {
+        val nums = intArrayOf(0,1,2,2,3,0,4,2)
+        val result = Solution().removeElement(nums, 2)
+
+        result shouldBe 5
+        nums.slice(0..result) shouldNotContain 2
     }
 })
