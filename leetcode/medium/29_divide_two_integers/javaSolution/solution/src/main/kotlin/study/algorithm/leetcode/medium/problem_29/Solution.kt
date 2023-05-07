584dc1a4bb56f5ee6239e48ee937a502bb97131a
Ddudooo <igoa.work@gmail.com>
2023-05-08 02:13:59 +0900

    add problem

---
+++ leetcode/medium/29_divide_two_integers/javaSolution/solution/src/main/kotlin/study/algorithm/leetcode/medium/problem_29/Solution.kt
@@ -0,0 +1,7 @@
+package study.algorithm.leetcode.medium.problem_29
+
+class Solution {
+    fun divide(dividend: Int, divisor: Int): Int {
+        TODO()
+    }
+}
---
+++ leetcode/medium/29_divide_two_integers/javaSolution/solution/src/test/kotlin/study/algorithm/leetcode/medium/problem_29/SolutionTest.kt
@@ -0,0 +1,10 @@
+package study.algorithm.leetcode.medium.problem_29
+
+import io.kotest.core.spec.style.StringSpec
+import org.junit.jupiter.api.Assertions.*
+
+class SolutionTest: StringSpec({
+    "정상적으로 인스턴스를 생성할 수 있어야 한다" {
+        assertDoesNotThrow { Solution() }
+    }
+})
