67b14c343975df788a6a106872fd0b5198ff76c3
Ddudooo <igoa.work@gmail.com>
2023-05-08 02:50:05 +0900

    add solution

--- leetcode/medium/29_divide_two_integers/javaSolution/solution/src/main/kotlin/study/algorithm/leetcode/medium/problem_29/Solution.kt
+++ leetcode/medium/29_divide_two_integers/javaSolution/solution/src/main/kotlin/study/algorithm/leetcode/medium/problem_29/Solution.kt
@@ -1,7 +1,15 @@
 package study.algorithm.leetcode.medium.problem_29
 
+import java.math.RoundingMode.DOWN
+import kotlin.math.pow
+
 class Solution {
     fun divide(dividend: Int, divisor: Int): Int {
-        TODO()
+        val MIN = (-2.0).pow(31).toBigDecimal()
+        val MAX = ((2.0).pow(31) - 1).toBigDecimal()
+
+        val result = dividend.toBigDecimal().divide(divisor.toBigDecimal(), 0, DOWN)
+
+        return result.max(MIN).min(MAX).toInt()
     }
 }
--- leetcode/medium/29_divide_two_integers/javaSolution/solution/src/test/kotlin/study/algorithm/leetcode/medium/problem_29/SolutionTest.kt
+++ leetcode/medium/29_divide_two_integers/javaSolution/solution/src/test/kotlin/study/algorithm/leetcode/medium/problem_29/SolutionTest.kt
@@ -1,10 +1,25 @@
 package study.algorithm.leetcode.medium.problem_29
 
 import io.kotest.core.spec.style.StringSpec
-import org.junit.jupiter.api.Assertions.*
+import io.kotest.matchers.shouldBe
 
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertDoesNotThrow
+
+@DisplayName("문제 풀이 요구사항")
 class SolutionTest: StringSpec({
     "정상적으로 인스턴스를 생성할 수 있어야 한다" {
         assertDoesNotThrow { Solution() }
+    }
+
+    "10, 3이 주어질때 반환 값으로는 3이 나와야 한다" {
+        Solution().divide(10, 3) shouldBe 3
+    }
+
+    "7, -3이 주어질때 반환 값으로는 -2가 나와야 한다" {
+        Solution().divide(7, -3) shouldBe -2
+    }
+    "-2147483648,-1이 주어질때 반환값으로 2147483648가 나와야 한다" {
+        Solution().divide(-2147483648, -1) shouldBe 2147483647
     }
 })
