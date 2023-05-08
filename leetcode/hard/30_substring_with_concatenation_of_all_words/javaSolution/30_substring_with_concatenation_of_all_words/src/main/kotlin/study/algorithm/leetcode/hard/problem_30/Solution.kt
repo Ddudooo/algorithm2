bb845c9020b7d003fe54ffbadcfd64e33822905e
Ddudooo <igoa.work@gmail.com>
2023-05-09 00:46:03 +0900

    add solution

--- leetcode/hard/30_substring_with_concatenation_of_all_words/javaSolution/30_substring_with_concatenation_of_all_words/src/main/kotlin/study/algorithm/leetcode/hard/problem_30/Solution.kt
+++ leetcode/hard/30_substring_with_concatenation_of_all_words/javaSolution/30_substring_with_concatenation_of_all_words/src/main/kotlin/study/algorithm/leetcode/hard/problem_30/Solution.kt
@@ -2,6 +2,32 @@ package study.algorithm.leetcode.hard.problem_30
 
 class Solution {
     fun findSubstring(s: String, words: Array<String>): List<Int> {
-        TODO()
+        val count = words[0].length
+        var left = 0
+        var right = (words.size * count)
+        val result = mutableListOf<Int>()
+
+        while(right <= s.length) {
+            val combination = s.substring(left, right).chunked(count).toMutableList()
+
+            if(isInclude(words, combination)) {
+                result.add(left)
+            }
+            left++
+            right++
+        }
+
+        return result
+    }
+
+    private fun isInclude(words: Array<String>, combination: MutableList<String>): Boolean {
+        for (word in words) {
+            if (!combination.contains(word)) {
+                return false
+            } else {
+                combination.remove(word)
+            }
+        }
+        return true
     }
 }
--- leetcode/hard/30_substring_with_concatenation_of_all_words/javaSolution/30_substring_with_concatenation_of_all_words/src/test/java/study/algorithm/leetcode/hard/problem_30/SolutionTest.kt
+++ leetcode/hard/30_substring_with_concatenation_of_all_words/javaSolution/30_substring_with_concatenation_of_all_words/src/test/java/study/algorithm/leetcode/hard/problem_30/SolutionTest.kt
@@ -1,10 +1,33 @@
 package study.algorithm.leetcode.hard.problem_30
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldHaveSize
 import org.junit.jupiter.api.assertDoesNotThrow
 
 class SolutionTest: StringSpec({
     "정상적으로 구성되어야 한다" {
         assertDoesNotThrow { Solution() }
+    }
+
+    "문자열이 barfoothefoobarman이고 단어배열이 foo,bar일때 반환값은 0,9가 나와야 한다" {
+        val result = Solution().findSubstring("barfoothefoobarman", arrayOf("foo", "bar"))
+
+        result shouldContain 0
+        result shouldContain 9
+    }
+
+    "문자열이 wordgoodgoodgoodbestword이고 단어 배열이 word, good, best, word일때 반환값은 비어있어야 한다" {
+        val result = Solution().findSubstring("wordgoodgoodgoodbestword", arrayOf("word", "good", "best", "word"))
+
+        result shouldHaveSize 0
+    }
+
+    "문자열이 barfoofoobarthefoobarman이고 단어 배열이 bar, foo, the일때 반환값이 6,9,12가 나와야 한다" {
+        val result = Solution().findSubstring("barfoofoobarthefoobarman", arrayOf("bar", "foo", "the"))
+
+        result shouldContain 6
+        result shouldContain 9
+        result shouldContain 12
     }
 })
