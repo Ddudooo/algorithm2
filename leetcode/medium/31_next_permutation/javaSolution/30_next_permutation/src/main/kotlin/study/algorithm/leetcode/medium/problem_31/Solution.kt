c61f016cd3d5c24c8e14c94f49e9c71a414cee3b
Ddudooo <igoa.work@gmail.com>
2023-05-10 00:53:16 +0900

    add solution

--- leetcode/medium/31_next_permutation/javaSolution/30_next_permutation/src/main/kotlin/study/algorithm/leetcode/medium/problem_31/Solution.kt
+++ leetcode/medium/31_next_permutation/javaSolution/30_next_permutation/src/main/kotlin/study/algorithm/leetcode/medium/problem_31/Solution.kt
@@ -1,7 +1,39 @@
 package study.algorithm.leetcode.medium.problem_31
 
 class Solution {
-    fun nextPermutation(nums: IntArray): Unit {
-        TODO()
+    fun nextPermutation(nums: IntArray) {
+
+        // 내림차순 정렬이 되지 않는 위치까지 찾음 - 1
+        var point = -1
+        for ( i in nums.lastIndex downTo 1){
+            if(nums[i] > nums[i-1]) {
+                point = i-1
+                break
+            }
+        }
+
+        // 정렬이 안되어 있으면
+        if(point >= 0) {
+            // 1에서 찾은 위치전까지 역순으로 순회하며 1에서 찾은 위치 값보다 큰값이 있을 경우 위치 변경
+            for (i in nums.lastIndex downTo point + 1) {
+                if (nums[point] < nums[i]) {
+                    nums.swap(point, i)
+                    break
+                }
+            }
+        }
+
+        // 전체 뒤집음
+        var left = point.inc()
+        var right = nums.lastIndex
+
+        while(left < right) {
+            nums.swap(left, right)
+            left = left.inc()
+            right = right.dec()
+        }
     }
+
+    // 위치 변환
+    private fun IntArray.swap(from: Int, to: Int) {this[from] = this[to].also { this[to] = this[from] }}
 }
--- leetcode/medium/31_next_permutation/javaSolution/30_next_permutation/src/test/kotlin/study/algorithm/leetcode/medium/problem_31/SolutionTest.kt
+++ leetcode/medium/31_next_permutation/javaSolution/30_next_permutation/src/test/kotlin/study/algorithm/leetcode/medium/problem_31/SolutionTest.kt
@@ -1,10 +1,36 @@
 package study.algorithm.leetcode.medium.problem_31
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainExactly
 import org.junit.jupiter.api.assertDoesNotThrow
+
 
 class SolutionTest: StringSpec({
     "정상적으로 생성되어야 한다" {
         assertDoesNotThrow { Solution() }
+    }
+
+    "입력값이 1,2,3일때 결과로 1,3,2가 나와야 한다" {
+        val nums : IntArray = intArrayOf(1,2,3)
+
+        Solution().nextPermutation(nums)
+
+        nums.toList().shouldContainExactly(listOf(1,3,2))
+    }
+
+    "입력값이 3,2,1 일때 결과로 1,2,3가 나와야 한다" {
+        val nums : IntArray = intArrayOf(3,2,1)
+
+        Solution().nextPermutation(nums)
+
+        nums.toList().shouldContainExactly(listOf(1,2,3))
+    }
+
+    "입력값이 1,1,5 일때 결과로 1,5,1가 나와야 한다" {
+        val nums : IntArray = intArrayOf(1,1,5)
+
+        Solution().nextPermutation(nums)
+
+        nums.toList().shouldContainExactly(listOf(1,5,1))
     }
 })
